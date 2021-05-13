163c1c65b416df18bd780110718c4df9b2651516
akhil <akhiltiwari.13@gmail.com>
2021-05-13 10:30:34 +0530

    adds assorted utilities

--- programs/main.cpp
+++ programs/main.cpp
@@ -1,14 +1,33 @@
-#include <log.h>
 #include <ConfigReader.h>
+#include <log.h>
+#include <memLayoutOrder.h>
 
 #include <string>
 
-/* Sample program which demostrate how to use most of the libraries in this project. */
-int main(int argc, char **argv){
-    /* @todo: read required data from a config file. */
-    std::string config("Config.ini");
-    Logger::getLogger().setDump(std::string("./.SampleLog"), false);
-    LOG_INFO("Sample Test Logging, successful!");
+using namespace com::elaeocarpus::assortedUtils;
 
-    return 0;
+class X {
+public:
+  int dataMem1;
+
+/* protected: */
+  int dataMem2;
+};
+
+/* friend char * access_order(data_type1 class_type::*mem1, data_type2
+ * class_type::*mem2); */
+
+/* Sample program which demostrate how to use most of the libraries in this
+ * project. */
+int main(int argc, char **argv) {
+  /* @todo: read required data from a config file. */
+  std::string config("Config.ini");
+  Logger::getLogger().setDump(std::string("./.SampleLog"), false);
+  LOG_INFO("Sample Test Logging, successful!");
+
+  /* X x; */
+
+  /* std::cout << access_order(&X::dataMem2, &X::dataMem1); */
+
+  return 0;
 }
